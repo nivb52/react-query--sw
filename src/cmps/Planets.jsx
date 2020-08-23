ea8f82b490090319969880b6d039922f0e34428e
niv b <nivb52@gmail.com>
2020-08-24 00:04:02 +0300

    small typos

--- src/cmps/Planets.jsx
+++ src/cmps/Planets.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+
 import {  rippleLoader, renderFetchError, renderMsgError } 
 from "./ui/LoadAndError.jsx";
+import Planet from "./Planet";
 
 const fetchPlanets = async () => {
   const res = await fetch('http://swapi.dev/api/planets');
@@ -10,9 +12,6 @@ const fetchPlanets = async () => {
 
 const Planets = () => {
   const { data, error , status } = useQuery('planets', fetchPlanets);
-  console.log('====================================');
-  console.log(data);
-  console.log('====================================');
   return (
     <>
       <h2>Planets</h2>
@@ -20,12 +19,14 @@ const Planets = () => {
 
   { status === 'loading' &&  (rippleLoader() )}
   { status === 'error' &&  (renderFetchError() )}
-  { data && typeof data === 'Object' && data.hasOwnProerty('results') 
-    && !data.resualts.length &&  renderMsgError('Not Found','warning') }
+  { data && typeof data === 'object' && data.hasOwnProperty('results') 
+    && !data.results.length &&  renderMsgError('Not Found','warning') }
 
     </div>
-    
-        <pre> {data && data.results && JSON.stringify(data.results, null, 2) } </pre>
+        {data && data.results && 
+         data.results.map ( planet =>  <Planet planet = {planet} key={planet.name}/>
+         )
+         } 
     </>
   );
 };
