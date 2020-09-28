c2a616808a37b90d382babe6456dd7d1ade2b1f9
niv b <nivb52@gmail.com>
2020-09-28 22:23:33 +0300

    adding dev tools

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from './cmps/Navbar';
 import Persons from './cmps/Persons';
 import Planets from './cmps/Planets';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 function App() {
   const [page, setPage] = React.useState();
   return (
+    <>
     <div className="app">
       <h1> Star Wars </h1>
       <Navbar setPage={setPage} />
@@ -13,6 +15,8 @@ function App() {
         {page === 'Persons' ? <Persons /> : <Planets />}
       </div>
     </div>
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </>
   );
 }
 
